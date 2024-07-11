8182cfe10aa91467f26524e5202ea28ca36e2770
Luis BC <luinux81@gmail.com>
2024-07-11 22:56:13 +0200

    Limpieza de código

--- src/index.php
+++ src/index.php
@@ -17,18 +17,19 @@
     $bodyClases = strtolower($activo->alias);
     
     if(JoomlaHelper::isCurrentViewCategory($app)){
-        $bodyClases .= " category-page ";
+        $bodyClases .= " category-page ";     
+        $isCategory = true;   
     }
 
     if(JoomlaHelper::isCurrentViewArticle($app)){
         $bodyClases .= " article-page ";
+        $isArticle = true;
     }
 
     if(JoomlaHelper::isCurrentViewHome($app)){
         $bodyClases .= " home-page ";
+        $isHome = true;
     }
-
-    $isHome = $activo == $menu->getDefault($lang->getTag());
     
 ?>
 
@@ -47,20 +48,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php
-        if($this->params['bootstrap']){
-            switch($this->params['bootstrapVersion']){
-                case "3":                    
-                    echo "<link href='https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu' crossorigin='anonymous'>";
-                    break;
-                case "4":                    
-                    echo "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>";
-                    break;  
-                case "5":
-                    echo "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' integrity='sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I' crossorigin='anonymous'>";
-                    break;
-            }
-        }
-
         if($this->params['googleFont']){
             $fuentes = explode(",",$this->params['googleFontName']);
             foreach($fuentes as $fuente){
@@ -143,26 +130,6 @@
             <jdoc:include type="modules" name="footer" />
         </div>
     </footer>
-
-<?php
-    if($this->params['bootstrap']){
-        switch($this->params['bootstrapVersion']){
-            case "3":
-                echo "<script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>";
-                echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>";                    
-                echo "<script src='https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js' integrity='sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd' crossorigin='anonymous'></script>";
-                break;
-            case "4":
-                echo "<script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>";
-                echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>";                    
-                echo "<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossorigin='anonymous'></script>";
-            break;
-            case "5":
-                echo "<script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin='anonymous'></script>";
-                echo "<script src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js' integrity='sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/' crossorigin='anonymous'></script>";
-        }
-    }
-?>
 
     <script type="text/javascript" src="<?php echo $templatePath . '/js/vendors.js' ?>"></script>
     <script type="text/javascript" src="<?php echo $templatePath . '/js/main.js' ?>"></script>    
